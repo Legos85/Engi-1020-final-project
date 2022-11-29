754c83851402a994f0141be8fd6140223e9b2f9e
Logan <116076514+Legos85@users.noreply.github.com>
2022-11-29 10:36:01 -0330

    Update Mode_1.py

--- Mode_1.py
+++ Mode_1.py
@@ -5,12 +5,14 @@ import Chest_opening
 def mode1():
     song_list = ["Mario underworld Theme (1)", "Zelda chest opening sound (2)", "Zelda's lullaby (3)"]
 
+    #asks user to input a value then checks to see if it the imput is valid
     print("to chose a song but input the coresponding number or if you link to go back to the select menu press(0)")
     what_song = input(f'{song_list} : ')
     while str.isnumeric(what_song) == False:
         print('Please entre a valid input')
         what_song = input(f'{song_list} : ')
 
+    #plays song based on the imput of the user
     if what_song == '1':
         return Mario_underworld_theme.underworld()
 
